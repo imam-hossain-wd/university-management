8c64258c9a32e2a345cec1706b42d344e84f9a67
imam-hossain <zunayedemon6t9@gmail.com>
2023-06-02 11:11:02 +0600

    interface and usermodel steup done

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,13 @@
 import app from './app'
 import mongoose from 'mongoose'
-const port = 5000
+import config from './config/index'
 
+const port = 8080
 const run = async () => {
   try {
     mongoose
-      .connect('mongodb://127.0.0.1:27017/test')
-      .then(() => console.log(`database connected successfully`))
+      .connect(config.database_url as string)
+      .then(() => console.log(`database is connected successfully`))
       .then(err => console.log(err))
   } catch (err) {
     console.log(err)
